393260285f69d660cff0bc68244021b9ba965236
arpank6 <arpank6@gmail.com>
2023-11-24 12:33:13 +0530

    24th Morning

--- src/test/java/com/bixi/BaseTestScripts/DebitCardTest.java
+++ src/test/java/com/bixi/BaseTestScripts/DebitCardTest.java
@@ -33,3 +33,32 @@ public class DebitCardTest extends BaseClass {
 
 	}
 }
+
+/**
+ * iRetryAnalyser execution
+ */
+//@Test(groups = "smoke",retryAnalyzer = com.bixi.genericutility.RetryAnalyserImp.class)
+//public void applyForDebitCardWithBlankFields_NeyazTC15() throws Throwable {
+//
+//	HomePage hp = new HomePage(driver);
+//	hp.getApplyDebitCardLink().click();
+//
+//	eUtil.readMultipleDataFromExcel("NeyazTC_15", driver);
+//
+//	Thread.sleep(2000);
+//	Debit_Card_FormPage dcf = new Debit_Card_FormPage(driver);
+//	dcf.getSubmitBtn().click();
+//
+//	String expText="No field should be empty";
+//	String actText = wUtil.getAlertPopUpText(driver);
+//	
+//	//Validation
+//	if(actText.equals(expText))
+//	System.out.println("Error message is Displayed and is Verified");
+//	else
+//		System.out.println("Error message not Displayed and Verified and Failed");
+//	Thread.sleep(5000);
+//	wUtil.handleAlertPopUp(driver);
+//
+//}
+
