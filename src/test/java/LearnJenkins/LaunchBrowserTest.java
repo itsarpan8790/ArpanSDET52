54df1ee49182d53bc6c3a014de1e01ea997a9c40
itsarpan8790 <arpank6@gmail.com>
2023-11-30 12:54:00 +0530

    gecko

---
+++ src/test/java/LearnJenkins/LaunchBrowserTest.java
@@ -0,0 +1,19 @@
+package LearnJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowserTest {
+	WebDriver driver;
+	
+	
+	@Test
+	public void test() {
+		
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
